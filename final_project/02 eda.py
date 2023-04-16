5afc1a45a892dc0e632bad71e154531537f4e2fc
GolliAch <GolliAch>
2023-04-16 22:38:43 +0000

    finished draft

--- final_project/02 eda.py
+++ final_project/02 eda.py
@@ -92,7 +92,7 @@ fig = go.Figure(
 # Set the axis labels and title
 fig.update_layout(
     xaxis_title='Hour of Day',
-    yaxis_title='Average Number of Trips',
+    yaxis_title='Total Number of Trips',
     title='Hourly Trip Trends'
 )
 
@@ -104,73 +104,137 @@ fig.show()
 
 import pandas as pd
 import seaborn as sns
-from pyspark.sql.functions import hour, count, avg
- 
-for month in range(1, 13):
-    month_df = mdf.toPandas().loc[mdf['relevant_time'].dt.month == month]
-    mdf1 = spark.createDataFrame(month_df)
-    hourly_trips = mdf1.groupBy(hour("relevant_time").alias("hour")).agg(count("*").alias("num_trips"))
-    hourly_trips = hourly_trips.withColumn("hour", hourly_trips["hour"].cast("int"))
-    hourly_trips = hourly_trips.groupBy("hour").agg(avg("num_trips").alias("avg_count"))
- 
-    # Convert the Spark DataFrame to a Pandas DataFrame
-    hourly_trips_pd = hourly_trips.toPandas()
- 
-    # Extract the desired column as a Pandas Series
-    hour_of_day = hourly_trips_pd['hour']
- 
-    # Filter the rows based on the current month
-    plot_data = hourly_trips_pd.loc[hour_of_day == month]
- 
-    # Plot the scatter plot
-    sns.scatterplot(x=plot_data['hour'], y=plot_data['avg_count'], label=month)
- 
+import matplotlib.pyplot as plt
+from pyspark.sql.functions import month, hour, count, sum
+
+# Create a new DataFrame with the month and hour of the trips
+mdf_month_hour = mdf.withColumn("month", month("relevant_time")).withColumn("hour", hour("relevant_time"))
+
+# Group trips by month, day, and hour, then count the number of trips
+trips_count = mdf_month_hour.groupBy("month", "hour", "relevant_time").agg(count("*").alias("num_trips"))
+
+# Group trips by month and hour, then sum the number of trips
+hourly_trips = trips_count.groupBy("month", "hour").agg(sum("num_trips").alias("total_trips"))
+
+# Convert the Spark DataFrame to a Pandas DataFrame
+hourly_trips_pd = hourly_trips.toPandas()
+
+# Create a dictionary to map month numbers to month names
+month_names = {
+    1: 'January', 2: 'February', 3: 'March', 4: 'April',
+    5: 'May', 6: 'June', 7: 'July', 8: 'August',
+    9: 'September', 10: 'October', 11: 'November', 12: 'December'
+}
+
+# Replace the month numbers with month names
+hourly_trips_pd['month'] = hourly_trips_pd['month'].map(month_names)
+
+# Add a new column with month numbers
+hourly_trips_pd['month_num'] = hourly_trips_pd['month'].map({v: k for k, v in month_names.items()})
+
+# Sort the DataFrame by the 'month_num' column
+hourly_trips_pd.sort_values('month_num', inplace=True)
+
+# Drop the 'month_num' column as it's no longer needed
+hourly_trips_pd.drop(columns=['month_num'], inplace=True)
+
+# Create the line plot
+plt.figure(figsize=(12, 6))
+sns.lineplot(data=hourly_trips_pd, x='hour', y='total_trips', hue='month', palette="Paired")
+
+# Set the axis labels and title
 plt.xlabel('Hour of Day')
-plt.ylabel('Average Number of Trips')
+plt.ylabel('Total Number of Trips')
 plt.title('Hourly Trip Trends by Month')
+
+# Move the legend to the right
+plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+
+# Display the plot
 plt.show()
 
 
 # COMMAND ----------
 
-from pyspark.sql.functions import date_format, count, col, mean
-import plotly.graph_objs as go
- 
+from pyspark.sql.functions import date_format, count, col, mean, hour, avg, max
+
 # Define list of common holidays
 common_holidays = ["01-01", "02-14", "03-17", "07-04", "08-31", "11-11", "12-25"]
+
 # Create a new column indicating whether the date is a common holiday or not
-filtered_mdf = filtered_mdf.withColumn("is_common_holiday", date_format("started_at", "MM-dd").isin(common_holidays))
- 
+filtered_mdf = mdf.withColumn("is_common_holiday", date_format("started_at", "MM-dd").isin(common_holidays))
+
 # Group trips by date and whether it's a common holiday or not
 daily_trips = filtered_mdf.groupBy("is_common_holiday", date_format("started_at", "MM-dd").alias("date")).agg(count("*").alias("num_trips"))
- 
+
 # Calculate the average number of trips per day for each category
 avg_daily_trips = daily_trips.groupBy("is_common_holiday").agg(mean(col("num_trips")).alias("avg_daily_trips"))
- 
-# Convert the Spark DataFrame to a Pandas DataFrame
+
+# Calculate trip duration in minutes
+filtered_mdf = filtered_mdf.withColumn("trip_duration_minutes", (col("ended_at").cast("long") - col("started_at").cast("long")) / 60)
+
+# Average trip duration for holidays and non-holidays
+avg_trip_duration = filtered_mdf.groupBy("is_common_holiday").agg(avg(col("trip_duration_minutes")).alias("avg_trip_duration"))
+
+# Calculate peak hours for holidays and non-holidays
+hourly_trips = filtered_mdf.withColumn("trip_hour", hour("started_at")).groupBy("is_common_holiday", "trip_hour").agg(count("*").alias("num_trips"))
+max_hourly_trips = hourly_trips.groupBy("is_common_holiday").agg(max("num_trips").alias("max_trips"))
+peak_hours = max_hourly_trips.join(hourly_trips, on=["is_common_holiday"], how="inner").filter(col("max_trips") == col("num_trips")).select("is_common_holiday", "trip_hour")
+
+# Convert the Spark DataFrames to Pandas DataFrames
 avg_daily_trips_pd = avg_daily_trips.toPandas()
- 
-# Create a bar chart
+avg_trip_duration_pd = avg_trip_duration.toPandas()
+peak_hours_pd = peak_hours.toPandas()
+
+# Create a bar chart for average daily trips
 fig = go.Figure(
     go.Bar(
         x=["Non-Holiday", "Common Holiday"],
         y=avg_daily_trips_pd['avg_daily_trips'],
     )
 )
- 
-# Set the axis labels and title
+
+# Set the axis labels and title for the bar chart
 fig.update_layout(
     xaxis_title='Day Type',
     yaxis_title='Average Number of Trips per Day',
     title='Daily Trip Trends: Common Holidays vs. Non-Holidays'
 )
- 
-# Display the plot
+
+# Display the bar chart
 fig.show()
+
+# Display the average trip duration and peak hours results
+print("Average Trip Duration (in minutes):")
+print(avg_trip_duration_pd)
+print("\nPeak Hours:")
+print(peak_hours_pd)
 
 
 # COMMAND ----------
 
+from pyspark.sql.functions import when, col
+from pyspark.sql.functions import hour, count, avg
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+mdf = mdf.withColumn("weather_category", col("main"))
+
+mdf_hour_weather = mdf.withColumn("hour", hour("relevant_time"))
+hourly_weather_trips = mdf_hour_weather.groupBy("hour", "weather_category").agg(count("*").alias("num_trips"))
+
+hourly_avg_weather_trips = hourly_weather_trips.groupBy("hour", "weather_category").agg(avg("num_trips").alias("avg_trips"))
+hourly_avg_weather_trips_pd = hourly_avg_weather_trips.toPandas()
+
+plt.figure(figsize=(16, 8))
+sns.lineplot(data=hourly_avg_weather_trips_pd, x='hour', y='avg_trips', hue='weather_category', palette="deep")
+
+plt.xlabel('Hour of Day')
+plt.ylabel('Average Number of Trips')
+plt.title('Hourly Trip Trends by Weather Condition')
+plt.legend(title="Weather Condition")
+
+plt.show()
 
 
 # COMMAND ----------
